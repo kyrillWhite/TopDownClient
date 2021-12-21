cd44edd9c5d531863ebd0deb11de79a8265d6cbe
kyrillWhite <kiribel2000@gmail.com>
2021-12-22 03:19:05 +0500

    half

--- TopDownGrpcClient/EventArgs/RetrieveUpdateEventArgs.cs
+++ TopDownGrpcClient/EventArgs/RetrieveUpdateEventArgs.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TopDownLibrary;
 
 namespace TopDownGrpcClient
 {
-    public class RetrieveEntitiesEventArgs : EventArgs
+    public class RetrieveUpdateEventArgs : EventArgs
     {
         public List<(string, int, float, float)> EntityPositions { get; set; }
+        public List<BulletData> Bullets { get; set; }
     }
 }
---
+++ TopDownLibrary/BulletData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopDownLibrary
+{
+    public class BulletData
+    {
+        public DateTime CreationTime { get; set; }
+        public float StartPosX { get; set; }
+        public float StartPosY { get; set; }
+        public float EndPosX { get; set; }
+        public float EndPosY { get; set; }
+        public int Team { get; set; }
+        public float Speed { get; set; }
+        public int Id { get; set; }
+    }
+}
