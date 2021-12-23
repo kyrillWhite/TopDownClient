afea97d23ef84d1639d2836d441e0d794b7378bc
olegase123 <olegase123@gmail.com>
2021-12-23 16:37:18 +0500

    Added direct join to game server

---
+++ TopDownWpfClient/ViewModels/JoinServerPageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+using TopDownWpfClient.Models;
+
+namespace TopDownWpfClient.ViewModels {
+	public class JoinServerPageViewModel : BaseViewModel {
+		public ICommand BackCommand { get; set; }
+
+		public event Action BackEvent;
+
+		public MainWindowViewModel MainWindowViewModel => MainWindowViewModel.Instance;
+
+		public ICommand JoinCommand { get; set; }
+
+		public event EventHandler<(string, int)> JoinedServer;
+
+		public string IP { get; set; }
+		public int Port { get; set; }
+
+		public JoinServerPageViewModel()
+		{
+			BackCommand = new DelegateCommand<object>(_ => BackEvent?.Invoke());
+			JoinCommand = new DelegateCommand<object>(_ => JoinedServer?.Invoke(this, (IP, Port)), _ => !string.IsNullOrEmpty(IP));
+		}
+	}
+}
